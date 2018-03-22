85c2c70522d855672049253372d2cc07a9848e50
Tainá <tainaraiodeluz@gmail.com>
2018-03-21 22:13:49 -0300

    State, Props and Change State

--- src/components/componentTest.jsx
+++ src/components/componentTest.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 class ComponentTest extends Component {
     render() {
+        const { myMessage } = this.props;
         return(
             <div>
                  <h3> My Component :) </h3>
+                 <h4> {myMessage} </h4>
             </div>
         );
     }
--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,29 @@ import ReactDOM, { render } from 'react-dom';
 import ComponentTest from './components/componentTest';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            msg: 'React State'
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() { 
+       this.setState({
+            msg: 'React State change'
+       });
+    }
+
     render() {
         return(
-          <ComponentTest />
+            <div>
+                <h3> {this.state.msg} </h3>
+                <ComponentTest myMessage={this.state.msg} />
+                <button onClick={this.handleClick}> Save </button>
+            </div>
         )
     }
 }
